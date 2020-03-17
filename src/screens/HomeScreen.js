e4cc44023cf3e850bef2ea51fc0568072506b9d0
B-raw <michaelbarry1989@hotmail.com>
2020-03-17 22:23:24 +0000

    Added navigation for Components and Lists Demo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,33 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello world people!</Text>;
+const HomeScreen = ({ navigation }) => {
+
+
+  return (
+    <View>
+      <Text style={styles.text}>Hello world people!</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => navigation.navigate("Components")}
+        color="green"
+        style={styles.button}
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")}
+        style={styles.button}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30
+  },
+  button: {
+    margin: 20
   }
 });
 
