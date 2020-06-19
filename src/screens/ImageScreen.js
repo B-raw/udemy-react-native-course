6cea677f6ba6a57383f2358fc6f8e00b1305cf7e
B-raw <michaelbarry1989@hotmail.com>
2020-06-19 20:27:44 +0100

    added photos and score to ImageDetail component

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -4,10 +4,21 @@ import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
   return <View>
-    <ImageDetail title="Forest" />
-    <ImageDetail title="Beach"/>
-    <ImageDetail title="Mountain"/>
-    <ImageDetail/>
+    <ImageDetail
+      title="Forest"
+      imageSource={require(`../../assets/forest.jpg`)}
+      score="9/10"
+    />
+    <ImageDetail
+      title="Beach"
+      imageSource={require(`../../assets/beach.jpg`)}
+      score="4/10"
+    />
+    <ImageDetail
+      title="Mountain"
+      imageSource={require(`../../assets/mountain.jpg`)}
+      score="8/10"
+    />
   </View>
 }
 
